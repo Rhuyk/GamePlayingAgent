b15e4f2fe3584b19a326a6e9ac2b3541f6c8d093
Richard Hu <rh20020421@gmail.com>
2024-04-24 00:55:50 +1200

    complete a fully customisable tic-tac-toe

--- TicTacToe/TTTGame.py
+++ TicTacToe/TTTGame.py
@@ -5,6 +5,7 @@ import numpy as np
 class TTTGame:
     rows = None
     cols = None
+    condition = None
     board = []
     players = ["X", "O"]
     player = players[0]
@@ -18,18 +19,18 @@ class TTTGame:
     frame = Frame(window)
     frame.pack()
 
-    def __init__(self, rows, cols):
+    def __init__(self, rows, cols, condition):
         self.rows = rows
         self.cols = cols
+        self.condition = condition
 
         for r in range(rows):
-            row_buttons = []
+            # row_buttons = []
             for c in range(cols):
-                button = Button(self.frame, text="", font=('consolas', 40), width=5, height=2,
+                button = Button(self.frame, text="", font=('consolas', 20), width=3, height=1,
                                 command=lambda row=r, column=c: self.next_turn(row, column))
                 button.grid(row=r, column=c)
-                row_buttons.append(button)
-            self.board.append(row_buttons)
+                self.board.append(button)
 
         reset_button = Button(text="restart", font=('consolas', 20), command=self.new_game)
         reset_button.pack(side="top")
@@ -37,10 +38,11 @@ class TTTGame:
         self.window.mainloop()
 
     def next_turn(self, row, col):
-        if self.board[row][col]['text'] == "" and self.check_winner() is False:
+        space = row * self.cols + col
+        if self.board[space]['text'] == "" and self.check_winner() is False:
 
             if self.player == self.players[0]:
-                self.board[row][col]['text'] = self.player
+                self.board[space]['text'] = self.player
                 if self.check_winner() is False:
                     self.player = self.players[1]
                     self.label.config(text=(self.players[1] + " turn"))
@@ -50,7 +52,7 @@ class TTTGame:
                     self.label.config(text="Tie!")
 
             else:
-                self.board[row][col]['text'] = self.player
+                self.board[space]['text'] = self.player
                 if self.check_winner() is False:
                     self.player = self.players[0]
                     self.label.config(text=(self.players[0] + " turn"))
@@ -60,36 +62,115 @@ class TTTGame:
                     self.label.config(text="Tie!")
 
     def check_winner(self):
-        for row in range(self.rows):
-            if self.board[row][0]['text'] == self.board[row][1]['text'] == self.board[row][2]['text'] != "":
-                self.board[row][0].config(bg="green")
-                self.board[row][1].config(bg="green")
-                self.board[row][2].config(bg="green")
-                return True
+        """Start from first column"""
+        for row in range(0, self.rows * self.cols, self.cols):
+            '''Check row'''
+            cond = 0
+            for i in range(self.cols):
+                space = row + i
+                if self.board[space]['text'] == self.player:
+                    cond += 1
+                    if cond == self.condition:
+                        for j in range(self.condition):
+                            self.board[space - j].config(bg="green")
+                        return True
+                else:
+                    cond = 0
 
-        for column in range(self.cols):
-            if self.board[0][column]['text'] == self.board[1][column]['text'] == self.board[2][column]['text'] != "":
-                self.board[0][column].config(bg="green")
-                self.board[1][column].config(bg="green")
-                self.board[2][column].config(bg="green")
-                return True
+            '''
+            Check downward diagonal
+            '''
+            cond = 0
+            r = None
+            for i in range(row, self.rows * self.cols, self.cols + 1):
+                space = i
+                if self.board[space]['text'] == self.player and (r is None or r == space // self.cols - 1):
+                    cond += 1
+                    r = space // self.cols
+                    if cond == self.condition:
+                        for j in range(self.condition):
+                            self.board[space - j * (self.cols + 1)].config(bg="green")
+                        return True
+                else:
+                    cond = 0
+                    r = None
 
-        if self.board[0][0]['text'] == self.board[1][1]['text'] == self.board[2][2]['text'] != "":
-            self.board[0][0].config(bg="green")
-            self.board[1][1].config(bg="green")
-            self.board[2][2].config(bg="green")
-            return True
+        """Start from first row"""
+        for col in range(self.cols):
+            '''
+            Check column
+            '''
+            cond = 0
+            for i in range(0, self.rows * self.cols, self.cols):
+                space = col + i
+                if self.board[space]['text'] == self.player:
+                    cond += 1
+                    if cond == self.condition:
+                        for j in range(self.condition):
+                            self.board[space - j * self.cols].config(bg="green")
+                        return True
+                else:
+                    cond = 0
 
-        elif self.board[0][2]['text'] == self.board[1][1]['text'] == self.board[2][0]['text'] != "":
-            self.board[0][2].config(bg="green")
-            self.board[1][1].config(bg="green")
-            self.board[2][0].config(bg="green")
-            return True
+            '''
+            Check downward diagonal
+            '''
+            cond = 0
+            r = None
+            for i in range(col, self.rows * self.cols, self.cols + 1):
+                space = i
+                if self.board[space]['text'] == self.player and (r is None or r == space // self.cols - 1):
+                    cond += 1
+                    r = space // self.cols
+                    if cond == self.condition:
+                        for j in range(self.condition):
+                            self.board[space - j * (self.cols + 1)].config(bg="green")
+                        return True
+                else:
+                    cond = 0
+                    r = None
 
-        elif self.empty_spaces() is False:
-            for row in range(self.rows):
-                for column in range(self.cols):
-                    self.board[row][column].config(bg="yellow")
+            '''
+            Check upward diagonal
+            '''
+            cond = 0
+            r = None
+            for i in range(col, -1, self.cols + 1):
+                space = i
+                if self.board[space]['text'] == self.player and (r is None or r == space // self.cols + 1):
+                    cond += 1
+                    r = space // self.cols
+                    if cond == self.condition:
+                        for j in range(self.condition):
+                            self.board[space - j * (self.cols + 1)].config(bg="green")
+                        return True
+                else:
+                    cond = 0
+                    r = None
+
+        """Start from last row"""
+        for col in range((self.rows - 1) * self.cols, self.rows * self.cols, 1):
+            '''
+            Check upward diagonal
+            '''
+            cond = 0
+            r = None
+            for i in range(col, -1, -(self.cols - 1)):
+                space = i
+                if self.board[space]['text'] == self.player and (r is None or r == space // self.cols + 1):
+                    cond += 1
+                    r = space // self.cols
+                    if cond == self.condition:
+                        for j in range(self.condition):
+                            self.board[space + j * (self.cols - 1)].config(bg="green")
+                        return True
+                else:
+                    cond = 0
+                    r = None
+
+        if self.empty_spaces() is False:
+            for space in range(self.rows * self.cols):
+                self.board[space].config(bg="yellow")
             return "Tie"
 
         else:
@@ -97,21 +178,18 @@ class TTTGame:
 
     def empty_spaces(self):
         spaces = self.rows * self.cols
-        for row in range(self.rows):
-            for column in range(self.cols):
-                if self.board[row][column]['text'] != "":
-                    spaces -= 1
+        for space in range(spaces):
+            if self.board[space]['text'] != "":
+                spaces -= 1
         return spaces != 0
 
     def new_game(self):
         self.player = self.players[0]
         self.label.config(text=self.player + " turn")
 
-        for row in range(self.rows):
-            for column in range(self.cols):
-                self.board[row][column].config(text="", bg="#F0F0F0")
+        for space in range(self.rows * self.cols):
+            self.board[space].config(text="", bg="#F0F0F0")
 
 
-ttt = TTTGame(3, 3)
-# ttt.print_board()
-# ttt.run_game()
+ttt = TTTGame(3, 3, 3)
+
